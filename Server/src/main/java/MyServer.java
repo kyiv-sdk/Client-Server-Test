c8586935a43fa54ca04d746051a5b91a86e5f5e8
Yuliia Tkachenko <ytkache@macmini5941.softservecom.com>
2020-03-11 17:11:14 +0200

    Update

--- Server/src/main/java/MyServer.java
+++ Server/src/main/java/MyServer.java
@@ -5,38 +5,84 @@ import java.util.Scanner;
 public class MyServer {
     public static void main(String[] args){
 
-
         try {
             ServerSocket ss = new ServerSocket(6666);
             Socket s = ss.accept();//establishes connection
             DataInputStream dis = new DataInputStream(s.getInputStream());
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-            while(true) {
-                String str = (String) dis.readUTF();
-                System.out.println("message= " + str);
 
-                if (str.equals("quit")) {
-                    ss.close();
-                    break;
-                }
+            InputThread inputThread = new InputThread(s, dis);
+            inputThread.start();
+            while(true) {
 
                 Scanner input = new Scanner(System.in);
                 System.out.print("Enter text to be send(" +"/" + "-/" +  "for end session): ");
-                String myString = input.next();
+                String toBeSend = input.next();
 
-                if (myString.equals("-")){
-                    dout.flush();
+                if (toBeSend.equals("-")){
+                    //dout.flush();
                     dout.close();
                     ss.close();
                     break;
                 }
-
-                dout.writeUTF(myString);
-                dout.flush();
-
+                OutputThread outputThread = new OutputThread(s, dout, toBeSend);
+                outputThread.start();
             }
+            inputThread.join();
         } catch (Exception e) {
             System.out.println(e);
+        }
+
+    }
+
+    static class InputThread extends Thread{
+
+        Socket socket;
+        DataInputStream dis;
+
+        InputThread(Socket s, DataInputStream dis){
+            this.dis = dis;
+            this.socket = s;
+        }
+        @Override
+        public void run() {
+            while (true){
+
+                String str = null;
+                try {
+                    str = (String) dis.readUTF();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("message= " + str);
+
+                if (str.equals("quit")) {
+                    break;
+                }
+            }
+            //close socket in main thread
+        }
+    }
+
+    static class OutputThread extends Thread{
+
+        Socket socket;
+        DataOutputStream dout;
+        String toBeSend;
+
+        OutputThread(Socket s, DataOutputStream dout, String message){
+            this.dout = dout;
+            this.socket = s;
+            toBeSend = message;
+        }
+        @Override
+        public void run() {
+            try {
+                dout.writeUTF(toBeSend);
+                dout.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
